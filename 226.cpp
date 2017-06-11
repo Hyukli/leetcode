56e08aded8e5f4621a7889c46d237a2ef438f170
Hyukli <1280096748@qq.com>
2017-06-11 11:49:53 +0800

    170611submit

---
+++ 226.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return root;
+        }
+        TreeNode* tmp;
+        //tmp=new TreeNode();
+        tmp=root->left;
+        root->left=root->right;
+        root->right=tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    kk.invertTree(Root);
+    queue<*TreeNode> q;
+
+    return 0;
+}
