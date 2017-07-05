8f215be39945d05e6c692662ebcfd55fc54b851b
Hyukli <1280096748@qq.com>
2017-07-05 15:30:57 +0800

    寻找二叉排序树两个节点的最近公共祖先（LCA）

---
+++ 235.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val>p->val&&root->val>q->val)
+        {
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        if(root->val<p->val&&root->val<q->val)
+        {
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        return root;
+    }
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Input the two value:"<<endl;
+    int pv,qv;
+    cin>>pv>>qv;
+    TreeNode *p=new TreeNode(pv);
+    TreeNode *q=new TreeNode(qv);
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution s;
+    cout<<s.lowestCommonAncestor(Root,p,q)->val<<endl;
+    return 0;
+}
