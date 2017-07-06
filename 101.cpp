e88d7f5dd0044d4644e6da83bbfaf52ac1564071
Hyuk Lee <1280096748@qq.com>
2017-07-06 09:47:07 +0800

    判断一棵树是否对称

---
+++ 101.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return root?isSymmetric(root->left,root->right):true;
+    }
+    bool isSymmetric(TreeNode *left,TreeNode *right)
+    {
+        if(left==NULL&&right==NULL)
+        {
+            return true;
+        }
+        if(left==NULL||right==NULL)
+        {
+            return false;
+        }
+        else
+        {
+            return (left->val==right->val&&isSymmetric(left->left,right->right)&&isSymmetric(left->right,right->left));
+        }
+    }
+};
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    cout<<(kk.isSymmetric(Root)?"Yes":"No"<<endl;
+    return 0;
+}
