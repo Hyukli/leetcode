582d553d03d8475f0bded748cce93317c5b9eb06
Hyuk Lee <1280096748@qq.com>
2017-07-10 18:02:26 +0800

    求一棵树每层的平均值

--- 637.cpp
+++ 637.cpp
@@ -1,12 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
@@ -24,7 +65,7 @@ public:
         return D;
     }
 private:
-    vector<vector<int>> V;
+    vector<vector<int> > V;
     int dfs(TreeNode* root,int n)
     {
         if(root==NULL)
@@ -49,3 +90,35 @@ private:
         }
     }
 };
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    vector<double> D;
+    D=kk.averageOfLevels(Root);
+    for(int i=0;i<D.size();i++)
+    {
+        cout<<D[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
