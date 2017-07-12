d58468cc200504f40a11be54843a0725330b97e3
Hyuk Lee <1280096748@qq.com>
2017-07-12 11:12:59 +0800

    判断一棵树是不是平衡二叉树

---
+++ 110.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(fabs(de(root->left,0)-de(root->right,0))>1)
+        {
+            return false;
+        }
+        return isBalanced(root->left)&&isBalanced(root->right);
+    }
+private:
+    int de(TreeNode *root,int n)
+    {
+        if(root==NULL)
+        {
+            return n;
+        }
+        else
+        {
+            return max(de(root->left,n+1),de(root->right,n+1));
+        }
+    }
+};
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    cout<<(kk.isBalanced(Root)?"Yes":"No")<<endl;
+    return 0;
+}
+
+
