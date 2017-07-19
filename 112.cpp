ac7845a3935c443d324ee2cfec08ac31a4b5e792
Hyuk Lee <1280096748@qq.com>
2017-07-19 19:34:45 +0800

    判断树中是否有某路径（根到叶子）和为某值

---
+++ 112.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+int a[1005];
+int n,sum;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root==NULL)
+        {
+            return false;
+        }
+        if(root->val==sum&&root->left==NULL&&root->right==NULL)
+        {
+            return true;
+        }
+        return(hasPathSum(root->right,sum-root->val)||hasPathSum(root->left,sum-root->val));
+    }
+};
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the sum:"<<endl;
+    cin>>sum;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    cout<<(kk.hasPathSum(Root,sum)?"Yes":"No")<<endl;
+    return 0;
+}
+
+
