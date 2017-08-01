756e095b98c969ad77c220211b1d799f2ebc23d5
Hyuk Lee <1280096748@qq.com>
2017-08-01 09:59:43 +0800

    Update 338.cpp

--- 338.cpp
+++ 338.cpp
@@ -8,7 +8,7 @@ public:
         vector<int> ans(num+1,0);
         for(int i=1;i<=num;i++)
         {
-            ans[i]=ans[i&(i-1)]+1;
+            ans[i]=ans[i&(i-1)]+1;//dp的含义为i和i-1相与，一定消除且唯一消除i的最后一个1
         }
         return ans;
     }
