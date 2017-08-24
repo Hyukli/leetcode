ee25e145e886ca8af3ba62a9f4966fb1d54fec63
Hyuk Lee <1280096748@qq.com>
2017-08-24 13:15:20 +0800

    在算式中添加括号输出所有可能的结果（分治法+记录重复值）

--- 241.cpp
+++ 241.cpp
@@ -5,14 +5,32 @@ using namespace std;
 
 class Solution {
 public:
+    map<string,vector<int>> m;
     vector<int> diffWaysToCompute(string input) {
         vector<int> ans;
         for(int i=0;i<input.size();i++)
         {
+            vector<int> left,right;
             if(input[i]=='+'||input[i]=='-'||input[i]=='*')
             {
-                vector<int> left=diffWaysToCompute(input.substr(0,i));
-                vector<int> right=diffWaysToCompute(input.substr(i+1));
+                if(m.find(input.substr(0,i))==m.end())
+                {
+                    left=diffWaysToCompute(input.substr(0,i));
+                    m[input.substr(0,i)]=left;
+                }
+                else
+                {
+                    left=m[input.substr(0,i)];
+                }
+                if(m.find(input.substr(i+1))==m.end())
+                {
+                    right=diffWaysToCompute(input.substr(i+1));
+                    m[input.substr(i+1)]=right;
+                }
+                else
+                {
+                    right=m[input.substr(i+1)];
+                }
                 for(int j=0;j<left.size();j++)
                 {
                     for(int k=0;k<right.size();k++)
