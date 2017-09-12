08d320f98c184926fed7214630291b4750cfba37
Hyuk Lee <1280096748@qq.com>
2017-09-12 14:03:52 +0800

    生成一个字符串集合，之后输入若干字符串，依次判断该字符串能否被集合中的某个元素替换一个字母后生成

--- 676.cpp
+++ 676.cpp
@@ -1,10 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
 class MagicDictionary {
 public:
     /** Initialize your data structure here. */
     MagicDictionary() {
 
     }
-    
+
     /** Build a dictionary through a list of words */
     void buildDict(vector<string> dict) {
         for(int i=0;i<dict.size();i++)
@@ -12,17 +17,18 @@ public:
             m[dict[i]]++;
         }
     }
-    
+
     /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
     bool search(string word) {
-        for(auto mm:m)
+        map<string,int>::iterator mm=m.begin();
+        for(;mm!=m.end();mm++)
         {
-            if(mm.first.size()==word.size())
+            if(mm->first.size()==word.size())
             {
                 int t=0;
                 for(int i=0;i<word.size();i++)
                 {
-                    if(mm.first[i]!=word[i])
+                    if(mm->first[i]!=word[i])
                     {
                         t++;
                     }
@@ -39,9 +45,24 @@ private:
     map<string,int> m;
 };
 
-/**
- * Your MagicDictionary object will be instantiated and called as such:
- * MagicDictionary obj = new MagicDictionary();
- * obj.buildDict(dict);
- * bool param_2 = obj.search(word);
- */
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    MagicDictionary obj =  MagicDictionary();
+    obj.buildDict(v);
+    int m;
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        string ss;
+        cin>>ss;
+        cout<<(obj.search(ss)?"Yes":"No")<<endl;
+    }
+    return 0;
+}
