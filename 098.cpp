6712fc6338ff7396f5f9a0f45cca5d24e3b89c0f
Hyuk Lee <1280096748@qq.com>
2018-02-01 16:29:36 +0800

    判断一棵二叉树是不是平衡二叉树

---
+++ 098.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+
+using namespace std;
+
+int a[1005];
+int n;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int load(int i,TreeNode *r)
+{
+    if(2*i+1<n)
+    {
+        if(a[2*i+1]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *p;
+            p=new TreeNode(0);
+            p->val=a[2*i+1];
+            r->left=p;
+            load(2*i+1,p);
+        }
+    }
+    if(2*i+2<n)
+    {
+        if(a[2*i+2]==0)
+        {
+
+        }
+        else
+        {
+            TreeNode *q;
+            q=new TreeNode(0);
+            q->val=a[2*i+2];
+            r->right=q;
+            load(2*i+2,q);
+        }
+    }
+    return 0;
+}
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return fun(root,INT_MIN,INT_MAX);
+    }
+private:
+    bool fun(TreeNode* root,long long mmin,long long mmax)
+    {
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(root->val>=mmax||root->val<=mmin)
+        {
+            return false;
+        }
+        return fun(root->left,mmin,root->val)&&fun(root->right,root->val,mmax);
+    }
+};
+
+int main()
+{
+    cout<<"please input the number of node:"<<endl;
+    cin>>n;
+    cout<<"please input the node one by one(use zero to replace null):"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    TreeNode *Root;
+    Root =new TreeNode(0);
+    if(!n)
+    {
+        Root=NULL;
+    }
+    else
+    {
+        Root->val=a[0];
+        load(0,Root);
+    }
+    Solution kk;
+    cout<<(kk.isValidBST(Root)?"Yes":"No")<<endl;
+    return 0;
+}
