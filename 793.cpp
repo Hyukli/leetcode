5c8015c41ce77f3487205ce009fd766e2b05fb54
Hyuk Lee <1280096748@qq.com>
2018-03-06 09:00:23 +0800

    二分法计算有几个数的竭诚最后有k个0

---
+++ 793.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int preimageSizeFZF(int K) {
+        return (int)(count(K)-count(K-1));
+    }
+    long long count(int n)
+    {
+        long long low=-1;
+        long long high=5000000000;
+        while(high-low>1)
+        {
+            long long h=high+low>>1;
+            long long s=0;
+            for(long long k=h/5;k>0;k/=5)
+            {
+                s+=k;
+            }
+            if(s<=n)
+            {
+                low=h;
+            }
+            else
+            {
+                high=h;
+            }
+        }
+        return low+1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    int n;
+    cin>>n;
+    cout<<s.preimageSizeFZF(n)<<endl;
+    return 0;
+}
